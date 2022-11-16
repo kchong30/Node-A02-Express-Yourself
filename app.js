eb1bb8fd9f7378ec99b5b051ae4fca39e3e4726f
Kevin Chong <kchong30@my.bcit.ca>
2022-11-16 01:08:14 -0800

    implemented profilesRouter

--- app.js
+++ app.js
@@ -5,12 +5,18 @@ const expressLayouts = require('express-ejs-layouts');
 const logger = require('morgan');
 const path = require('path');
 const bodyParser = require('body-parser');
+const profilesRouter = require("./routers/profilesRouter");
 
 const port = process.env.PORT || 3003;
 const app = express();
 
-app.listen(port, () => console.log(`Example app listening on port ${port}`));
+app.use(logger("dev"));
+
+app.use("/profiles", profilesRouter);
+
 
 app.get('*', function(req, res) {
     res.status(404).send('<h2 class = "error">File Not Found</h2>');
 })
+
+app.listen(port, () => console.log(`Example app listening on port ${port}`));
