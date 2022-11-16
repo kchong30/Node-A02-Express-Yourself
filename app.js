885febe29bc01559d313ac5c0dc8b07cc9ca1970
Kevin Chong <kchong30@my.bcit.ca>
2022-11-16 02:37:52 -0800

    added views for about, profile, profiles

--- app.js
+++ app.js
@@ -7,13 +7,25 @@ const path = require('path');
 const bodyParser = require('body-parser');
 const profilesRouter = require("./routers/profilesRouter");
 const indexRouter = require("./routers/indexRouter");
+const apiRouter = require("./routers/apiRouter");
 
 const port = process.env.PORT || 3003;
 const app = express();
-
 app.use(logger("dev"));
 
-app.use("/", indexRouter);
+app.use(express.static("public"));
+
+app.use(expressLayouts);
+app.set("layout", "./layouts/full-width");
+
+app.set ("views", path.join(__dirname,"view"));
+
+app.set("view engine", "ejs");
+
+app.use (bodyParser.urlencoded({extended: false}));
+app.use (bodyParser.json());
+
+app.use(indexRouter);
 app.use("/profiles", profilesRouter);
 
 
