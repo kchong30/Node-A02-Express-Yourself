8105e0263ebb833a5d5c80fe7653c8c54e4e76e0
Kevin Chong <kchong30@my.bcit.ca>
2022-11-16 03:13:42 -0800

    added specific profiles implementation in profiles router. (drawn from class room walkthrough)

--- routers/profilesRouter.js
+++ routers/profilesRouter.js
@@ -27,4 +27,34 @@ profilesRouter.get("/", (req, res) => {
       });
   });
 
+  profilesRouter.get("/:id", (req, res) => {
+    fs.readFile(dataPath + "profiles.json")
+      .then((contents) => {
+        console.log(contents);
+        // need to parse the raw buffer as json if we want to work with it
+        const profilesJson = JSON.parse(contents);
+        console.log(profilesJson);
+        // sort the objects by name
+        profilesJson.sort((a, b) =>
+          a.name > b.name ? 1 : b.name > a.name ? -1 : 0
+        );
+        const profileJson = profilesJson
+          .filter((profile) => profile.id === req.params.id)
+          .shift();
+        console.log(profileJson);
+        //   prepare and send an OK response
+        res.render("profile", {
+          title: "Express Yourself - " + profileJson.name,
+          profiles: profilesJson,
+          profileId: req.params.id,
+          layout: "./layouts/sidebar",
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+        res.writeHead(500);
+        res.end("Error");
+      });
+  });
+
 module.exports = profilesRouter;
