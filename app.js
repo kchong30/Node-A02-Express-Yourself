e14052e7d378e0d449c41aa88f9da66426b4f3f2
Kevin Chong <kchong30@my.bcit.ca>
2022-11-16 02:42:12 -0800

    modified app.js file added express static, set layout, views

--- app.js
+++ app.js
@@ -22,9 +22,6 @@ app.set ("views", path.join(__dirname,"view"));
 
 app.set("view engine", "ejs");
 
-app.use (bodyParser.urlencoded({extended: false}));
-app.use (bodyParser.json());
-
 app.use(indexRouter);
 app.use("/profiles", profilesRouter);
 
